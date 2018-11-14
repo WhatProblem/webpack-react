e04e518414682557ab8b899c024da307564b2d31
WhatProblem <18355350915@163.com>
2018-11-14 22:42:33 +0800

    基础架构提交

--- src/app/auth/Auth.js
+++ src/app/auth/Auth.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 export class Auth extends React.Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
+        console.log(this.props)
         const { location, config } = this.props;
         const { pathname } = location;
         const isLogin = localStorage.getItem('myToken');
@@ -10,29 +14,33 @@ export class Auth extends React.Component {
         const targetRouterConfig = config.find(v => v.path === pathname);
         if (targetRouterConfig && !targetRouterConfig.auth && !isLogin) {
             const { component } = targetRouterConfig;
-            return <Route exact path = { pathname }
-            component = { component }
+            return <Route exact path={pathname}
+                component={component}
             />
         }
 
         if (isLogin) {
             if (pathname === '/login') {
-                return <Redirect to = "/" / >
+                return <Redirect to="/" />
             } else {
+                console.log(11111111111111111111111111);
+                console.log(targetRouterConfig)
                 if (targetRouterConfig) {
-                    return <Route path = { pathname }
-                    component = { targetRouterConfig.component }
+                    return <Route path={pathname}
+                        component={targetRouterConfig.component}
                     />
                 } else {
-                    return <Redirect to = '/404' / >
+                    return <Redirect to='/404' />
                 }
             }
         } else {
             if (targetRouterConfig && targetRouterConfig.auth) {
-                return <Redirect to = "/login" / >
+                return <Redirect to="/login" />
             } else {
-                return <Redirect to = "/404" / >
+                return <Redirect to="/404" />
             }
         }
     }
 }
+
+// https://blog.csdn.net/zwkkkk1/article/details/83411071
--- src/app/router/Router.js
+++ src/app/router/Router.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { HashRouter, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, HashRouter, Switch } from 'react-router-dom';
 import { Auth } from '../auth/Auth';
 import { routerConfig } from "./Routes";
+
+// export default class AppRouter extends React.Component {
+//     render() {
+//         return (
+//             <HashRouter>
+//                 <Switch>
+//                     <Auth config={routerConfig} />
+//                 </Switch>
+//             </HashRouter>
+//         );
+//     }
+// }
 
 export default class AppRouter extends React.Component {
     render() {
         return (
-            <HashRouter>
+            <Router>
                 <Switch>
                     <Auth config={routerConfig} />
                 </Switch>
-            </HashRouter>
+            </Router>
         );
     }
 }
